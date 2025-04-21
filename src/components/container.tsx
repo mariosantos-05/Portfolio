d02e30bd6a098e3cf3e65fdc9089b9b8115b497f
mariosantos-05 <marioaugustov.s@gmail.com>
2025-04-21 18:53:57 -0300

    container component, first test

---
+++ src/components/container.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from 'react';
+
+
+interface ContainerBoxProps {
+    name: string;
+    onClose: () => void;
+    children?: ReactNode;
+}
+
+export default function ContainerBox({ name, onClose, children }: ContainerBoxProps) {
+    return (
+        <div className="relative bg-[#D7F3FF] rounded-[22px] shadow w-full max-w-sm  outline outline-[4px] outline-[#1DA4A4] outline-offset-[0px]">
+            <div className="relative w-full max-w-sm h-[40px] bg-[#1DA4A4] rounded-t-[22px]">
+
+                {/* Close button */}
+                <button
+                    onClick={onClose}
+                    className="absolute right-[5%] top-[15%] text-[#D7F3FF] hover:text-white text-lg font-bold rounded-full  transition duration-300 ease-in-out"
+                >
+                    ×
+                </button>
+
+                {/* Title */}
+                <h2 className="absolute left-[5%] top-[15%]">{name}</h2>
+            </div>
+            {/* Optional children */}
+            <div className=" m-3 text-gray-700">
+                {children}
+            </div>
+        </div>
+    );
+}
